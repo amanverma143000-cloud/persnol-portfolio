f0e2246ac850882c0dccd2685c04d0d7dde20072
amanverma143000-cloud <amanverma143000@gmail.com>
2025-09-13 15:23:01 +0530

    error resolve

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-// server/server.js
-/* eslint-env node */
-/* global process */
+// server.js
 import express from "express";
 import nodemailer from "nodemailer";
 import cors from "cors";
@@ -9,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -24,8 +22,8 @@ app.post("/api/contact", async (req, res) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER, // FIXED
-        pass: process.env.EMAIL_PASS, // FIXED
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
@@ -48,5 +46,5 @@ app.post("/api/contact", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
